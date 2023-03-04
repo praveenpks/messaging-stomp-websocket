e6902620677ce6bf69de5f48277d2a54efbe5a9f
PraveenPKS <praveenkle12@gmail.com>
2023-03-04 20:00:25 +0530

    websocket example commit

---
+++ src/main/java/com/example/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.messagingstompwebsocket;
+
+public class Greeting {
+	
+	private String content;
+
+	public Greeting() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Greeting(String content) {
+	   this.content = content;
+	}
+
+	public String getContent() {
+	   return content;
+	}
+
+}
---
+++ src/main/java/com/example/messagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,23 @@
+package com.example.messagingstompwebsocket;
+
+public class HelloMessage {
+	
+	private String name;
+
+	public HelloMessage() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public HelloMessage(String name) {
+	    this.name = name;
+	}
+
+	public String getName() {
+	    return name;
+	}
+
+	public void setName(String name) {
+	    this.name = name;
+	}
+
+}
